2bf0e711236ecd0de89aa6de81a78ca0232ac02c
carlos <cganoza@minsa.gob.pe>
2019-10-24 18:07:40 -0500

    [IMP] Se extraen los campos completos de los popup.

--- scraping_ubigeo_inei/main.py
+++ scraping_ubigeo_inei/main.py
@@ -20,31 +20,35 @@ def main():
 
         if get_value(dist):
             td = 4
-            print('%s %s %s' % (get_value(depa), get_value(prov), get_value(dist)))
+            # print('%s %s %s' % (get_value(depa), get_value(prov), get_value(dist)))
         elif get_value(prov):
             td = 3
-            print('%s %s' % (get_value(depa), get_value(prov)))
+            # print('%s %s' % (get_value(depa), get_value(prov)))
         else:
             td = 2
-            print(get_value(depa))
+            # print(get_value(depa))
             code, value = get_value(depa).split(' ', 1)
             values[code] = value
 
         try:
-            tble_trs = tree.xpath('/html/body/table[2]/tbody/tr[%s]/td[%s]/div/table/tr'% (tr, td))
+            tble_trs = tree.xpath('/html/body/table[2]/tbody/tr[%s]/td[%s]/div/table/tr' % (tr, td))
             ptable = {}
             for table_tr in range(1, (len(tble_trs) + 1)):
-                key = tree.xpath('/html/body/table[2]/tbody/tr[%s]/td[%s]/div/table/tr[%s]/td[1]/text()'% (tr, td, table_tr))
-                value = tree.xpath('/html/body/table[2]/tbody/tr[%s]/td[%s]/div/table/tr[%s]/td[3]/text()'% (tr, td, table_tr))
-                key = ' '.join(key[0].split()) if len(key) else ''
-                value = ' '.join(value[0].split()) if len(value) else ''
-                ptable[key] = value
+                key = False
+                for i in [1, 3, 4, 6]:
+                    temp = tree.xpath('/html/body/table[2]/tbody/tr[%s]/td[%s]/div/table/tr[%s]/td[%s]/text()' % (tr, td, table_tr, i))
+                    temp = ' '.join(temp[0].split()) if len(temp) else ''
+                    if key:
+                        ptable[key] = temp
+                        key = False
+                    else:
+                        key = temp
             if len(ptable):
                 print(ptable)
 
         except Exception as esc:
             print(esc)
-    print(values)
+    # print(values)
 
 
 if __name__ == '__main__':
